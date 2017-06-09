b6bcb463c703527cbd463904ea4169e4e436d2ce
krzysiekjekaterynczuk <k.jekaterynczuk@gmail.com>
2017-06-09 15:07:41 +0200

    class Book and db dump

---
+++ api/rest_db.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Linux (x86_64)
+--
+-- Host: localhost    Database: rest_db
+-- ------------------------------------------------------
+-- Server version	5.7.17-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_polish_ci NOT NULL,
+  `author` varchar(255) COLLATE utf8_polish_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8_polish_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books`
+--
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES (1,'W pustyni i w puszczy','Henryk Sienkiewicz','Książka opowiadająca o przygodach Stasia i Nell'),(2,'Dragon Ball tom 1.','Akira Toriyama','Pierwsza część mangi opowidającej o przygodach Son Goku'),(3,'Poza schematem. Sekrety ludzi sukcesu','Malcolm Gladwell','Co składa się na sukces? Niekonwencjonalne odpowiedzi.');
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-03-07 20:22:23
---
+++ api/src/Book.php
@@ -0,0 +1,108 @@
+<?php
+
+class Book {
+    
+    private $id;
+    private $title;
+    private $author;
+    private $description;
+    
+    function __construct() {
+        $this->id = -1;
+        $this->title = "";
+        $this->author = "";
+        $this->description = "";
+    }
+    
+    function getId() {
+        return $this->id;
+    }
+    
+    function getTitle() {
+        return $this->title;
+    }
+
+    function getAuthor() {
+        return $this->author;
+    }
+
+    function getDescription() {
+        return $this->description;
+    }
+
+    function setTitle($title) {
+        $this->title = $title;
+    }
+
+    function setAuthor($author) {
+        $this->author = $author;
+    }
+
+    function setDescription($description) {
+        $this->description = $description;
+    }
+
+    static function loadFromDB(mysqli $conn, $id = null) {
+        $results = [];
+        
+        if (!is_null($id)) {
+            $id = $conn->real_escape_string($id);
+            $sql = "SELECT * FROM books WHERE id =$id";
+        } else {
+            $sql = "SELECT * FROM books";
+        }
+        
+        $result = $conn->query($sql);
+        
+        while($row = $result->fetch_assoc()) {
+            $results[] = $row;
+        }
+
+        return $results;
+    }
+    
+    public function create(mysqli $conn, $title, $author, $description = null) {
+        $title = $conn->real_escape_string($title);
+        $author = $conn->real_escape_string($author);
+        $description = $conn->real_escape_string($description);
+        
+        if (strlen(trim($description)) == 0) {
+            $sql = 'INSERT INTO books VALUES (null, "'.$title.'","'.$author.'","'.$description.'")';
+        } else {
+            $sql = 'INSERT INTO books VALUES (null, "'.$title.'","'.$author.'")';
+        }
+        
+        $result = $conn->query($sql);
+        
+        if ($result === TRUE) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }   
+    }
+    
+    public function update(mysqli $conn, $id, $title, $author, $description) {
+        $id = $conn->real_escape_string($id);
+        $title = $conn->real_escape_string($title);
+        $author = $conn->real_escape_string($author);
+        $description = $conn->real_escape_string($description);
+        
+        $sql = 'UPDATE books
+            SET title="'.$title.'", author="'.$author.'", description="'.$description.'"
+            WHERE id='.$id.';';
+        
+        $result = $conn->query($sql);
+        
+        return $result;
+    }
+    
+    public function deleteFronDB(mysqli $conn, $id) {
+        $id = $conn->real_escape_string($id);
+        $sql = 'DELETE FROM books WHERE id='.$id.';';
+        
+        $result = $conn->query($sql);
+        
+        return $result;
+    }
+}
+
