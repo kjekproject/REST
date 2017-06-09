eef95320e10ce50f57f74b8376d22eb61166d7a0
krzysiekjekaterynczuk <k.jekaterynczuk@gmail.com>
2017-06-09 15:40:25 +0200

    basic layout

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+
+?>
+
+<html>
+    <head>
+        <title>BookStore</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="js/script.js"></script>
+        <link rel="stylesheet" href="./style/style.css"/>
+    </head>
+    <body>
+        <nav class="navbar navbar-fixed-top navbar-inverse">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">BookStore</a>
+                </div>
+            </div><!-- /.container -->
+        </nav>
+        
+        <div class="container myContent">
+            <div class="row row-offcanvas row-offcanvas-right">
+                <div class="col-xs-12 col-sm-12">
+                    <div class="jumbotron">
+                        <h2>Add a new book</h2><br/>                   
+                        <form onsubmit="return false;">
+                            <div class="form-group">
+                                <label for="titleInput">Title</label>
+                                <input name="title" type="text" class="form-control" id="titleInput">
+                            </div>
+                            <div class="form-group">
+                                <label for="authorInput">Author</label>
+                                <input name="title" type="text" class="form-control" id="authorInput">
+                            </div>
+                            <div class="form-group">
+                                <label for="descriptionInput">Description</label>
+                                <textarea name="description" class="form-control" rows="1" id="descriptionInput"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Add book</button>
+                        </form>
+                    </div>
+                  
+                    <div id="book-list" class="row">
+                        '<div class="col-xs-6 col-lg-4">
+                            <h2>Heading</h2>
+                            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                            <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+                        </div>'
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <footer>
+            <p class="text-center">© 2017 KJ_Project</p>
+        </footer>
+        
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    </body>
+</html>
+
